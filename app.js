260d1d4dcf36c92bc208fd1f341bdc1ea8d02226
rutvik17 <patelrutvik1702@gmail.com>
2023-08-30 17:51:54 -0400

    update detail page collections page app.js

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const fetch = require('node-fetch')
 const prismic = require('@prismicio/client')
 const prismicH = require('@prismicio/helpers')
 
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+const logger = require('morgan')
+
 const accessToken = process.env.PRISMIC_ACCESS_TOKEN
 const baseURL = process.env.PRISMIC_ENDPOINT
 
@@ -39,7 +43,11 @@ const HandleLinkResolver = (doc) => {
   return '/'
 }
 
+app.use(logger('dev'))
 app.use(errorHandler())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride())
 
 // Add a middleware function that runs on every route. It will inject
 // the prismic context to the locals so that we can access these in
@@ -53,48 +61,78 @@ app.use((req, res, next) => {
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+const handleRequest = async (api) => {
+  const meta = await api.getSingle('meta')
+  const navigation = await api.getSingle('navigation')
+  const preloader = await api.getSingle('preloader')
+  return {
+    meta,
+    navigation,
+    preloader
+  }
+}
+
 app.get('/', async (req, res) => {
   // Here we are retrieving the first document from your API endpoint
-  res.render('pages/home')
+  const api = initApi(req)
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
+  const collections = await api.getAllByType(
+    'collection', { fetchLinks: 'product.image' }
+  )
+
+  res.render('pages/home', {
+    ...defaults,
+    collections,
+    home
+  })
 })
 
 app.get('/about', async (req, res) => {
   const api = initApi(req)
-  const [meta, about] = await Promise.all([
+  const [meta, about, navigation, preloader] = await Promise.all([
     api.getSingle('meta'),
-    api.getSingle('about')
+    api.getSingle('about'),
+    api.getSingle('navigation'),
+    api.getSingle('preloader')
   ])
   // api.get(
   //   prismic.predicate.any('document.type', ['about', 'meta'])
   // )
+
+  console.log(navigation)
   res.render('pages/about', {
     about,
-    meta
+    meta,
+    navigation,
+    preloader
   })
 })
 
 app.get('/collections', async (req, res) => {
   const api = initApi(req)
-  const meta = await api.getSingle('meta')
+  const defaults = await handleRequest(api)
+  const home = await api.getSingle('home')
   const collections = await api.getAllByType(
-    'collection'
+    'collection', { fetchLinks: 'product.image' }
   )
-  console.log(collections)
+
   res.render('pages/collections', {
-    meta,
-    collections
+    ...defaults,
+    collections,
+    home
   })
 })
 
-app.get('/item/:uid', async (req, res) => {
+app.get('/detail/:uid', async (req, res) => {
   const api = initApi(req)
-  const meta = await api.getSingle('meta')
+  const defaults = await handleRequest(api)
   const product = await api.getByUID('product', req.params.uid, {
     fetchLinks: 'collection.title'
   })
 
-  res.render('pages/item', {
-    meta,
+  res.render('pages/detail', {
+    ...defaults,
     product
   })
 })
